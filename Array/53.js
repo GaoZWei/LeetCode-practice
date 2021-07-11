6d5fbb90f55109c5d9c044df7665e3bfe7a29f17
Gao <1520045270@qq.com>
2021-07-11 14:14:29 +0800

    最大子序和+动态规划

--- Array/53.js
+++ Array/53.js
@@ -5,32 +5,46 @@
 // 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
 
 
-// 思路一: 如果i的前一个是小于零,不添加
+// 思路一: 贪心算法 
 // 逐渐往后遍历,找到当前的最优解
 var maxSubArray = function (nums) {
     var pre = 0;
     var maxItem = nums[0]; //存储最大的和
     for (let i = 0; i < nums.length; i++) {
-        pre = Math.max(pre + nums[i], nums[i])
+        // 等价于 f(i)=max(f(i-1)+nums[i],nums[i])    用  f(i) 代表以第 i 个数结尾的「连续子数组的最大和」!!!  
+        pre = Math.max(pre + nums[i], nums[i]) //动态规划转移方程 
+        // console.log(pre)
         maxItem = Math.max(pre, maxItem)
     }
     return maxItem
 };
 
-//思路二:贪心算法  若当前指针所指元素之前的和小于0,则丢弃当前元素之前的数列
+//思路二:贪心算法  若当前指针所指元素之前的和小于0,则丢弃当前元素之前的数列   (思路一 与 思路二 等价)
 var maxSubArray = function (nums) {
     var sum = 0;
-    var ans = nums[0]
+    var maxItem = nums[0]
     for (let i = 0; i < nums.length; i++) {
-        if (sum > 0) { //如果之前的sum>0
+        if (sum > 0) { //如果之前的sum>0,增益
             sum += nums[i]
         } else {
             sum = nums[i]
         }
-        ans = Math.max(ans, sum)
+        maxItem = Math.max(maxItem, sum)
     }
-    return ans
+    return maxItem
 }
 
+//思路三:动态规划   如果i的前一个是大于零,添加
+var maxSubArray = function (nums) {
+    var len = nums.length
+    var maxItem = nums[0];
+    for (let i = 1; i < len; i++) {
+        if (nums[i - 1] > 0) {
+            nums[i] += nums[i - 1]
+        }
+        maxItem = Math.max(maxItem, nums[i])
+    }
+    return maxItem
+}
 var nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 console.log(maxSubArray(nums))
