325c54cfe518e85a93d8ee2db7c0da85b0ef372f
Gao <1520045270@qq.com>
2021-12-01 14:24:51 +0800

    链表专题2

---
+++ CodeSystem/LinkList/3-1-0207.js
@@ -0,0 +1,31 @@
+// 面试题 02.07. 链表相交
+// 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+
+var getLinkLen = (head) => {
+    var cur = head
+    var len = 0
+    while (cur) {
+        len++
+        cur = cur.next
+    }
+    return len
+}
+var getIntersectionNode = function (headA, headB) {
+    var lenA = getLinkLen(headA)
+    var lenB = getLinkLen(headB)
+    var curA = headA,
+        curB = headB
+    if (lenA < lenB) {
+        [curA, curB] = [curB, curA];
+        [lenA, lenB] = [lenB, lenA];
+    }
+    var i = lenA - lenB
+    while (i-- > 0) {
+        curA = curA.next
+    }
+    while (curA && curA != curB) {
+        curA = curA.next
+        curB = curB.next
+    }
+    return curA
+};
