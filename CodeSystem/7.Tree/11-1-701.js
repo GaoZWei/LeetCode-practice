ba88597bbbafab5d3e7decbd1136db0f9014b08f
Gao <1520045270@qq.com>
2022-01-02 16:07:24 +0800

    二叉树专题11(操作树)

--- CodeSystem/7.Tree/11-1-701.js
+++ CodeSystem/7.Tree/11-1-701.js
@@ -18,3 +18,53 @@ var insertIntoBST = function (root, val) {
     }
     return setInOrder(root, val)
 }
+
+//无返回值的递归
+var insertIntoBST = function (root, val) {
+    var parent = new TreeNode(0)
+    var preOrder = (cur, val) => {
+        if (cur == null) {
+            var node = new TreeNode(val)
+            if (parent.val > val) {
+                parent.left = node
+            } else {
+                parent.right = node
+            }
+            return
+        }
+        parent = cur
+        if (cur.val > val) {
+            preOrder(cur.left, val)
+        } else {
+            preOrder(cur.right, val)
+        }
+    }
+    if (root == null) root = new TreeNode(val)
+    preOrder(root, val)
+    return root
+}
+
+//迭代
+var insertIntoBST = function (root, val) {
+    if (root == null) {
+        root = new TreeNode(val)
+    } else {
+        var parent = new TreeNode(0)
+        var cur = root
+        while (cur) {
+            parent = cur
+            if (cur.val > val) {
+                cur = cur.left
+            } else {
+                cur = cur.right
+            }
+        }
+        var node = new TreeNode(val)
+        if (parent.val > val) {
+            parent.left = node
+        } else {
+            parent.right = node
+        }
+    }
+    return root
+}
