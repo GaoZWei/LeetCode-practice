fbe0b487a8fdf977474acb846229ea8620207073
Gao <1520045270@qq.com>
2022-04-11 16:38:18 +0800

    改变this指向

---
+++ 2.Algorithm/13.js
@@ -0,0 +1,41 @@
+// 改变this指向
+
+// 指向window
+function foo() {
+    console.log(this);
+}
+foo() //window
+
+// 1.call
+let bar = {
+    myname: 'abc',
+    test1: 1
+}
+
+function foo1() {
+    this.myname = "bcd"
+}
+foo1.call(bar)
+console.log(bar);
+
+// 2.对象调用方法设置
+var obj = {
+    name: "极客时间",
+    showThis: function () {
+        console.log(this);
+    }
+}
+obj.showThis()
+
+// 3.构造函数中设置
+function createObj() {
+    this.name = "极客时间"
+    // console.log(this);
+}
+var myboj = new createObj()
+console.log(myboj); //createObj { name: '极客时间' }
+
+// new等价于
+// var tmpObj={}
+// createObj.call(tmpObj)
+// return tmpObj
